a7ea15e124137df22469f651602a73063fa92530
Oleksandr <zagrebelniysasha@gmail.com>
2023-11-29 13:17:57 +0200

    Updated comments

--- drop-tabs.php
+++ drop-tabs.php
@@ -1,8 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Тут виконайте видалення всіх даних на сервері (файли, БД тощо).
 
-    // Приклад видалення даних з файлу:
     file_put_contents('tabs.json', json_encode([]));
 
     // Redirect back to second.php
--- fetch-tabs.php
+++ fetch-tabs.php
@@ -1,12 +1,8 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'GET' || $_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Handle both GET and POST requests
-
-    // Example: Load data from a file
     $data = json_decode(file_get_contents('tabs.json'), true);
     echo json_encode($data);
 } else {
-    // Respond with a 405 Method Not Allowed for other request methods
     http_response_code(405);
 }
 ?>
